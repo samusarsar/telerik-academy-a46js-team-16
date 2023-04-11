4164c3c0404e2dec1314f57240b389c6add8d3e6
samu.sar <samuil.sarandev@gmail.com>
2023-04-11 16:16:17 +0300

    Add Home view and home nav event

--- gif-project/src/events/navigation-events.js
+++ gif-project/src/events/navigation-events.js
@@ -1,4 +1,5 @@
-import { CONTAINER_SELECTOR, TRENDING } from '../common/constants.js';
+import { CONTAINER_SELECTOR, HOME, TRENDING } from '../common/constants.js';
+import { toHomeView } from '../views/home-view.js';
 import { toTrendingView } from '../views/trending-view.js';
 import { setActiveNav } from './helpers.js';
 
@@ -6,6 +7,9 @@ export const loadPage = (page = '') => {
 
   switch (page) {
 
+  case HOME:
+    setActiveNav(HOME);
+    return renderHome();
   case TRENDING:
     setActiveNav(TRENDING);
     return renderTrending();
@@ -16,8 +20,13 @@ export const loadPage = (page = '') => {
 
 };
 
+export const renderHome = () => {
+  document.querySelector(CONTAINER_SELECTOR).innerHTML = toHomeView();
+};
+
 export const renderTrending = async () => {
   document.querySelector(CONTAINER_SELECTOR).innerHTML = await toTrendingView();
 };
+
 
 // renderTrending();
--- gif-project/src/views/home-view.js
+++ gif-project/src/views/home-view.js
@@ -1,13 +1,14 @@
 export const toHomeView = () => `
 <div id="home">
-  <h1>Mov(e)ster</h1>
+  <h1>GIFlamingo</h1>
   <div class="content">
-    <p>Simple movie database app. You can:</p>
+    <p>Your one-stop-shop for the best GIFs out there!</p>
     <ul>
-      <li>Browse categories</li>
-      <li>Browse movies</li>
-      <li>Add and remove movies from favorites</li>
-      <li>Search for movies by title</li>
+      <li>Check out the newest, trending GIFs</li>
+      <li>Search any specific GIF title that comes to mind</li>
+      <li>Choose your favorite GIF to express yourself</li>
+      <li>Upload your very own GIFs</li>
+      <li>Show off your GIFlamingo Profile with all of your uploads</li>
     </ul>
   </div>
 </div>
