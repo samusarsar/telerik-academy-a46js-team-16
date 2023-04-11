b3d9b2df6043f2a064bd7050ec60e9c157af6ec6
samu.sar <samuil.sarandev@gmail.com>
2023-04-11 19:36:05 +0300

    Add change data-gif-id on toMiniGifView

--- gif-project/src/views/gif-views.js
+++ gif-project/src/views/gif-views.js
@@ -1,5 +1,5 @@
 export const toMiniGifView = (gif) => `
   <div class="gif-box">
-    <a id="${gif.id}"><img src="${gif.images.fixed_height_downsampled.url}" alt="${gif.title}"></a>
+    <a data-gif-id="${gif.id}"><img src="${gif.images.fixed_height_downsampled.url}" alt="${gif.title}"></a>
   </div>
 `;
