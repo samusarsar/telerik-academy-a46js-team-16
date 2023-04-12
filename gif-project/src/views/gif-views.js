483233c59acea6bd170054f6bd0f401cc06f4821
Simona Ilcheva <sdmileva@gmail.com>
2023-04-12 11:49:34 +0300

    add toDetailedGifView / Views

--- gif-project/src/views/gif-views.js
+++ gif-project/src/views/gif-views.js
@@ -1,5 +1,16 @@
+import { EMPTY_HEART } from "../common/constants.js";
+
 export const toMiniGifView = (gif) => `
   <div class="gif-box">
-    <a data-gif-id="${gif.id}"><img src="${gif.images.fixed_height_downsampled.url}" alt="${gif.title}"></a>
+    <a href=#><img class="mini-gif-img" data-gif-id="${gif.id}" src="${gif.images.fixed_height_downsampled.url}" alt="${gif.title}"></a>
   </div>
 `;
+
+export const toDetailedGifView = (gif) => {
+  console.log(gif)
+  return `<h3>${gif.title}</h3>
+  <img src="${gif.images.original.url}">
+  <p>Uploaded by: ${gif.user?.display_name || gif.username || 'anonymous user'}</p>`
+}
+
+{/* <p>Uploaded by: ${gif.user.display_name}, username: ${gif.username}</p> */}
