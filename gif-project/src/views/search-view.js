2eb1601da2857f6afbf8414f1b460776be7f79d9
Simona Ilcheva <sdmileva@gmail.com>
2023-04-14 10:46:52 +0300

    refactor toSearchView()

--- gif-project/src/views/search-view.js
+++ gif-project/src/views/search-view.js
@@ -1,19 +1,15 @@
 import { toMiniGifView } from './gif-views.js';
 
-export const toSearchView = (gifs, searchTerm) => {
-  if (!searchTerm || !searchTerm.trim()) {
-    return `<div id="gifs">
+export const toSearchView = (gifs, searchTerm) => `
+    <div id="gifs">
         <h1>Gifs found for"${searchTerm}"</h1>
         <div class="content">
-            <p>Enter words to search GIFs</p>
+            ${(searchTerm || searchTerm.trim()) ? toSuccessfulSearchView(gifs) : toNotSuccessfulSearchView()}
         </div>
     </div>
 `;
-  } return `<div id="gifs">
-    <h1>Gifs found for"${searchTerm}"</h1>
-    <div class="content">
-        ${gifs.map(toMiniGifView).join('\n') || `<p>No GIFs found</p>`}
-    </div>
-</div>
-`;
-};
+
+const toSuccessfulSearchView = (gifs) => gifs.map(toMiniGifView).join('\n') || `<p>No GIFs found</p>`;
+
+const toNotSuccessfulSearchView = () => `<p>Enter words to search GIFs</p>`;
+
