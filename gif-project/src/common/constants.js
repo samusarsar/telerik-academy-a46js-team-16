da9ebec9bc8e6c4f34ba5838e293611a7120bb2a
samu.sar <samuil.sarandev@gmail.com>
2023-04-16 21:27:38 +0300

    Add lint and unused files cleanup

--- gif-project/src/common/constants.js
+++ gif-project/src/common/constants.js
@@ -13,10 +13,15 @@ export const CONTAINER_SELECTOR = '#container';
 export const API_KEY = 'p7p1CiPHdMbNcBaSRaZBJJnQ9GC5udb5';
 
 export const NAV_LINK = 'nav-link';
+
 export const DATA_PAGE = 'data-page';
+
 export const FAV_BUBBLE = 'favorite-bubble';
+
 export const SUBMIT = 'submit';
+
 export const FILE_ID = 'file';
+
 export const EMPTY_NEST = 'empty-nest';
 
 export const MINI_GIF_IMG = 'mini-gif-img';
