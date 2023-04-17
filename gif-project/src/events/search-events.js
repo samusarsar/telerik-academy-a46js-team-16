8c13b50e7ed8e1edda0e6caff2d1014c3d2ff535
Simona Ilcheva <sdmileva@gmail.com>
2023-04-17 14:51:54 +0300

    refactor search_events.js

--- gif-project/src/events/search-events.js
+++ gif-project/src/events/search-events.js
@@ -1,56 +1,19 @@
-import { API_KEY, CONTAINER_SELECTOR, GIF_BOX, SEARCH_CONTENT } from '../common/constants.js';
+import { CONTAINER_SELECTOR, SEARCH_CONTENT } from '../common/constants.js';
 import { searchGifs } from '../requests/request-service.js';
-import { toMiniGifView } from '../views/gif-views.js';
+import { generateSearchGifsUrl } from '../requests/url-generators.js';
 import { toErrorView } from '../views/interface-views.js';
 import { toSearchView } from '../views/search-view.js';
-import { setActiveNav } from './helpers.js';
+import { applyInfiniteScroll, applyMasonry, setActiveNav } from './helpers.js';
 
 export const renderSearchItems = async (searchTerm) => {
-  let counter = 0;
-
   try {
     setActiveNav();
     const gifs = await searchGifs(searchTerm);
     document.querySelector(CONTAINER_SELECTOR).innerHTML = toSearchView(gifs, searchTerm);
 
-    const msnryContainer = document.querySelector(SEARCH_CONTENT);
+    const msnry = applyMasonry(SEARCH_CONTENT);
 
-    const msnry = new Masonry(msnryContainer, {
-      initLayout: false,
-      itemSelector: GIF_BOX,
-      columnWidth: 200,
-      gutter: 10,
-      fitWidth: true,
-    });
-
-    imagesLoaded(msnryContainer, () => {
-      msnry.layout();
-    });
-
-    const infScroll = new InfiniteScroll(msnryContainer, {
-      path: function() {
-        return `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchTerm}&limit=20&offset=${counter+=20}`;
-      },
-      responseBody: 'json',
-      outlayer: msnry,
-      status: '.page-load-status',
-      history: false,
-      scrollThreshold: 250,
-    });
-
-    const proxyElem = document.createElement('div');
-
-    infScroll.on('load', function( body ) {
-      const itemsHTML = body.data.map( toMiniGifView ).join('');
-      proxyElem.innerHTML = itemsHTML;
-      const items = proxyElem.querySelectorAll(GIF_BOX);
-      imagesLoaded( items, function() {
-        infScroll.appendItems( items );
-        msnry.appended( items );
-      });
-    });
-
-    infScroll.loadNextPage();
+    applyInfiniteScroll(SEARCH_CONTENT, msnry, generateSearchGifsUrl, searchTerm);
 
   } catch (error) {
     document.querySelector(CONTAINER_SELECTOR).innerHTML = toErrorView();
