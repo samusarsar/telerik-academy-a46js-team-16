9cd1e9d3556962955be1d954dc54ac3ec13acfb0
samu.sar <samuil.sarandev@gmail.com>
2023-04-27 17:08:05 +0300

    Add initial folders and dummy data structure

---
+++ interiorum-directory/interiorum/data.js
@@ -0,0 +1,52 @@
+const data = [
+    {
+        id: 1,
+        title: 'hello',
+        body: 'fsdjkhfjklshafdlkhsakdjlfhkljsdafh',
+        images: [
+            url1,
+            url2,
+            url3,
+        ],
+        publishedOn: '01/02/2022',
+        author: {
+            id: 1,
+            name: 'Emma J',
+        },
+        category: 'Bathroom',
+        tags: [
+            'carpet',
+            'couch',
+        ],
+        comments: [
+            {
+                text: 'gsdfagfsadfsadf',
+                author: 'John K',
+                authorId: 2,
+                likes: 12,
+                saves: 10,
+                publishedOn: '01/02/2022',
+            },
+            {
+                text: 'dfasfsdaf',
+                author: 'John T',
+                authorId: 3,
+                likes: 12,
+                saves: 10,
+                publishedOn: '01/02/2022',
+
+            },
+            {
+                text: 'gsfhdghdfgh',
+                author: 'John R',
+                authorId: 4,
+                likes: 12,
+                saves: 10,
+                publishedOn: '01/02/2022',
+
+            },
+        ],
+        likes: 20,
+        saves: 30,
+    },
+];
