32314d87881fc0402e13a4194aa31e67a1038270
samu.sar <samuil.sarandev@gmail.com>
2023-04-28 13:02:37 +0300

    Add chakra library and wrapped app

--- interiorum-directory/interiorum/src/main.jsx
+++ interiorum-directory/interiorum/src/main.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+
+import { ChakraProvider } from '@chakra-ui/react';
+
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <ChakraProvider>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </ChakraProvider>
     </React.StrictMode>,
 );
