c509cf335fc33af69a641702d569e1b9f4f3c175
samu.sar <samuil.sarandev@gmail.com>
2023-04-28 23:08:18 +0300

    Add more dummy data for posts and users

--- interiorum-directory/interiorum/data.js
+++ interiorum-directory/interiorum/data.js
@@ -49,6 +49,156 @@ export const posts = [
         likes: 20,
         saves: 30,
     },
+    {
+        id: 2,
+        title: 'hello',
+        body: 'fsdjkhfjklshafdlkhsakdjlfhkljsdafh',
+        images: [
+            'url1',
+            'url2',
+            'url3',
+        ],
+        publishedOn: '01/02/2022',
+        author: {
+            id: 1,
+            name: 'Emma J',
+        },
+        category: 'Bathroom',
+        tags: [
+            'carpet',
+            'couch',
+        ],
+        comments: [
+            {
+                text: 'gsdfagfsadfsadf',
+                author: 'John K',
+                authorId: 2,
+                likes: 12,
+                saves: 10,
+                publishedOn: '01/02/2022',
+            },
+            {
+                text: 'dfasfsdaf',
+                author: 'John T',
+                authorId: 3,
+                likes: 12,
+                saves: 10,
+                publishedOn: '01/02/2022',
+
+            },
+            {
+                text: 'gsfhdghdfgh',
+                author: 'John R',
+                authorId: 4,
+                likes: 12,
+                saves: 10,
+                publishedOn: '01/02/2022',
+
+            },
+        ],
+        likes: 20,
+        saves: 30,
+    },
+    {
+        id: 3,
+        title: 'hello',
+        body: 'fsdjkhfjklshafdlkhsakdjlfhkljsdafh',
+        images: [
+            'url1',
+            'url2',
+            'url3',
+        ],
+        publishedOn: '01/02/2022',
+        author: {
+            id: 1,
+            name: 'Emma J',
+        },
+        category: 'Bathroom',
+        tags: [
+            'carpet',
+            'couch',
+        ],
+        comments: [
+            {
+                text: 'gsdfagfsadfsadf',
+                author: 'John K',
+                authorId: 2,
+                likes: 12,
+                saves: 10,
+                publishedOn: '01/02/2022',
+            },
+            {
+                text: 'dfasfsdaf',
+                author: 'John T',
+                authorId: 3,
+                likes: 12,
+                saves: 10,
+                publishedOn: '01/02/2022',
+
+            },
+            {
+                text: 'gsfhdghdfgh',
+                author: 'John R',
+                authorId: 4,
+                likes: 12,
+                saves: 10,
+                publishedOn: '01/02/2022',
+
+            },
+        ],
+        likes: 20,
+        saves: 30,
+    },
+    {
+        id: 4,
+        title: 'hello',
+        body: 'fsdjkhfjklshafdlkhsakdjlfhkljsdafh',
+        images: [
+            'url1',
+            'url2',
+            'url3',
+        ],
+        publishedOn: '01/02/2022',
+        author: {
+            id: 1,
+            name: 'Emma J',
+        },
+        category: 'Bathroom',
+        tags: [
+            'carpet',
+            'couch',
+        ],
+        comments: [
+            {
+                text: 'gsdfagfsadfsadf',
+                author: 'John K',
+                authorId: 2,
+                likes: 12,
+                saves: 10,
+                publishedOn: '01/02/2022',
+            },
+            {
+                text: 'dfasfsdaf',
+                author: 'John T',
+                authorId: 3,
+                likes: 12,
+                saves: 10,
+                publishedOn: '01/02/2022',
+
+            },
+            {
+                text: 'gsfhdghdfgh',
+                author: 'John R',
+                authorId: 4,
+                likes: 12,
+                saves: 10,
+                publishedOn: '01/02/2022',
+
+            },
+        ],
+        likes: 20,
+        saves: 30,
+    },
 ];
 
 export const users = [{
