4d96dcbb9532ca47d5570de1ef98dabfb26377bd
samu.sar <samuil.sarandev@gmail.com>
2023-04-29 10:36:35 +0300

    Extend default theme with brand colors

--- interiorum-directory/interiorum/src/main.jsx
+++ interiorum-directory/interiorum/src/main.jsx
@@ -3,13 +3,25 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 
 import './index.css';
 
+const theme = extendTheme({ 
+    colors: {
+        brand: {
+            100: '#DBCCBD',
+            200: '#D49A6F',
+            300: '#B14C31',
+            400: '#444A53',
+            500: '#021825',
+            600: '#f5f5f5',
+        },
+    } });
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <ChakraProvider>
+        <ChakraProvider theme={theme} >
             <BrowserRouter>
                 <App />
             </BrowserRouter>
