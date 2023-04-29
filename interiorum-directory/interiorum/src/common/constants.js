ae4646a370e5cdd50c75a7a50247fd2a76433c86
Simona Ilcheva <sdmileva@gmail.com>
2023-04-29 12:10:39 +0300

    add constants

---
+++ interiorum-directory/interiorum/src/common/constants.js
@@ -0,0 +1,14 @@
+export const USER_NAME_MIN_LENGTH = 4;
+export const USER_NAME_MAX_LENGTH = 32;
+export const ADMIN_NAME_MIN_LENGTH = 4;
+export const ADMIN_NAME_MAX_LENGTH = 32;
+export const POST_TITLE_MIN_LENGTH = 16;
+export const POST_TITLE_MAX_LENGTH = 64;
+export const POST_CONTENT_MIN_LENGTH = 32;
+export const POST_CONTENT_MAX_LENGTH = 8192;
+
+export const BRAND_COLOR_1 = '#DBCCBD';
+export const BRAND_COLOR_2 = '#D49A6F';
+export const BRAND_COLOR_3 = '#B14C31';
+export const BRAND_COLOR_4 = '#444A53';
+export const BRAND_COLOR_5 = '#021825';
