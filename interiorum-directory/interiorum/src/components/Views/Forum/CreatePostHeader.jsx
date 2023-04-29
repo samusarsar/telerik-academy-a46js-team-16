b066877bb7c5c068e9024a10920e2b7881e031b5
Simona Ilcheva <sdmileva@gmail.com>
2023-04-29 16:28:52 +0300

    add client side validation for post title & content

--- interiorum-directory/interiorum/src/components/Views/Forum/CreatePostHeader.jsx
+++ interiorum-directory/interiorum/src/components/Views/Forum/CreatePostHeader.jsx
@@ -1,23 +1,49 @@
-import { Box, Collapse, useDisclosure, InputGroup, Input, InputRightAddon, Heading, Text, Center } from '@chakra-ui/react'
+import { Box, Collapse, useDisclosure, InputGroup, Input, Heading, Text, FormControl, FormErrorMessage, Alert, AlertIcon, AlertTitle } from '@chakra-ui/react'
 
 import CreatePostView from './CreatePostView';
-import { POST_TITLE_MAX_LENGTH } from '../../../common/constants';
+import { POST_TITLE_MAX_LENGTH, POST_TITLE_MIN_LENGTH } from '../../../common/constants';
+import { useState } from 'react';
 
 const CreatePostHeader = () => {
 
     const { isOpen, onToggle } = useDisclosure();
+
+    const [postTitleLength, setPostTitleLength] = useState(true);
+
+    const postTitleLengthIsValid = (input) => {
+        console.log(input.value.length)
+        if((input.value.length < POST_TITLE_MIN_LENGTH || input.value.length > POST_TITLE_MAX_LENGTH) && input.value.length > 0) {
+            setPostTitleLength(false)
+        } else {
+            setPostTitleLength(true)
+        }
+    }
 
     return (
         <Box p='80px 50px' w='100%' className='create-post-header' bgImage="url('src/assets/images/moderninteriordesign.jpg')" bgPosition="center" opacity='70%'>
             <Heading m='10px auto' textAlign='center'>What are you working on?</Heading>
             <Text m='10px auto' textAlign='center'>Get help for your projects, share your finds, and show off your Before & After.</Text>
             <InputGroup>
-                <Input m='10px 0' bg='white' onClick={onToggle} focusBorderColor='brand.400' placeholder='Enter your question' />
-                <InputRightAddon m='10px 0' children={POST_TITLE_MAX_LENGTH} />
+                <FormControl isInvalid={!postTitleLength} >
+                    {!postTitleLength ? (
+
+                        <FormErrorMessage color='red' opacity="90%" backgroundColor={'white'} m='0'>
+                            <Alert status='error'>
+                                <AlertIcon />
+                                <AlertTitle>Post title should be between {POST_TITLE_MIN_LENGTH} and {POST_TITLE_MAX_LENGTH} symbols</AlertTitle>
+                            </Alert>
+
+                        </FormErrorMessage>
+                    ) : (
+                        <></>
+                    )}
+                    <Input onChange={(e) => postTitleLengthIsValid(e.target)} m='0px 0px 10px' bg='white' onClick={onToggle} focusBorderColor='brand.400' placeholder='Enter your question' />
+
+                </FormControl>
             </InputGroup>
             <Collapse in={isOpen} animateOpacity>
                 <Box>
-                <CreatePostView />
+                    <CreatePostView />
                 </Box>
             </Collapse>
         </Box>
--- interiorum-directory/interiorum/src/components/Views/Forum/CreatePostView.jsx
+++ interiorum-directory/interiorum/src/components/Views/Forum/CreatePostView.jsx
@@ -1,22 +1,46 @@
-import { Textarea, Stack, Button, Checkbox, Collapse, useDisclosure } from '@chakra-ui/react';
+import { Textarea, Stack, Button, Checkbox, Collapse, useDisclosure, FormErrorMessage, Alert, AlertIcon, AlertTitle, FormControl, Box } from '@chakra-ui/react';
 import { categories } from '../../../../data';
+import { POST_CONTENT_MAX_LENGTH, POST_CONTENT_MIN_LENGTH } from '../../../common/constants';
+import { useState } from 'react';
 
 const CreatePostView = () => {
 
     const { isOpen, onToggle } = useDisclosure();
 
+    const [postContentLength, setPostContentLength] = useState(true);
+
+    const postContentLengthIsValid = (input) => {
+        if ((input.value.length < POST_CONTENT_MIN_LENGTH || input.value.length > POST_CONTENT_MAX_LENGTH) && input.value.length > 0) {
+            setPostContentLength(false);
+        } else {
+            setPostContentLength(true);
+        }
+    };
+
     return (
-        <Stack>
-            <Textarea bg="white" focusBorderColor='brand.400' size='lg' placeholder='Tell us the specific details here' />
-            <Button onClick={onToggle}>Select category</Button>
+        <Box>
+            <FormControl isInvalid={!postContentLength} >
+                {!postContentLength ? (
+                    <FormErrorMessage color='red' opacity="90%" backgroundColor={'white'} m='0'>
+                        <Alert status='error'>
+                            <AlertIcon />
+                            <AlertTitle>Post content should be between {POST_CONTENT_MIN_LENGTH} and {POST_CONTENT_MAX_LENGTH} symbols</AlertTitle>
+                        </Alert>
+                    </FormErrorMessage>
+                ) : (
+                    <></>
+                )}
+            </FormControl>
+            <Textarea onChange={e => postContentLengthIsValid(e.target)} bg="white" focusBorderColor='brand.400' size='lg' placeholder='Tell us the specific details here' />
+            <Button onClick={onToggle} width='100%' marginTop='10px'>Select category</Button>
             <Collapse in={isOpen} animateOpacity>
-                <Stack borderRadius='10px' padding='10px' marginBottom='10px' bg='white'>
+                <Stack borderRadius='10px' padding='10px' margin='10px 0' bg='white'>
                     {categories.map(c => <Checkbox iconColor='brand.400' colorScheme='black' key={c}>{c}</Checkbox>)}
                 </Stack>
                 <Button width='100%'>Create</Button>
 
             </Collapse>
-        </Stack>
+        </Box>
     );
 };
 
