583e5580b4bd2865dafc867bee89cc99c1b19011
samu.sar <samuil.sarandev@gmail.com>
2023-05-01 13:56:59 +0300

    Add verification for SignUp component

--- interiorum-directory/interiorum/src/components/Views/LogIn-SignUp/SignUp.jsx
+++ interiorum-directory/interiorum/src/components/Views/LogIn-SignUp/SignUp.jsx
@@ -1,27 +1,53 @@
 import { FormControl, FormLabel, Input, FormErrorMessage, Box, Heading, Text, Button, HStack, Divider, VStack, Flex } from '@chakra-ui/react';
 import { useContext, useState } from 'react';
 import { AuthContext } from '../../../AuthContext/AuthContext';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { UserContext } from '../../../UserContext/UserContext';
+import { useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
-    const user = useContext(AuthContext);
+    const status = useContext(AuthContext);
+    const user = useContext(UserContext);
+    console.log(user);
     const navigate = useNavigate();
-    const location = useLocation();
-
-    const from = location.state?.from || '/';
 
     const [firstName, setFirstName] = useState('');
+    const [firstNameError, setFirstNameError] = useState(false);
     const [lastName, setLastName] = useState('');
+    const [lastNameError, setLastNameError] = useState(false);
     const [username, setUsername] = useState('');
+    const [usernameError, setUsernameError] = useState(false);
     const [email, setEmail] = useState('');
+    const [emailError, setEmailError] = useState(false);
     const [password, setPassword] = useState('');
+    const [passwordError, setPasswordError] = useState(false);
+    const [rePassword, setRePassword] = useState('');
+    const [rePasswordError, setRePasswordError] = useState(false);
 
-    const handleFirstNameChange = (e) => setFirstName(e.target.value);
-    const handleLastNameChange = (e) => setLastName(e.target.value);
-    const handleUsernameChange = (e) => setUsername(e.target.value);
-    const handleEmailChange = (e) => setEmail(e.target.value);
-    const handlePasswordChange = (e) => setPassword(e.target.value);
-    const isError = !email.includes('@');
+    const handleSignUp = () => {
+        // setUsernameError(snapshot.exists());
+        setPasswordError(password.length < 6);
+        setRePasswordError(rePassword !== password);
+        setFirstNameError(firstName.length < 4 || firstName.length > 32);
+        setLastNameError(lastName.length < 4 || lastName.length > 32);
+        // setEmailError(!email.includes('@') || snapshot.exists());
+        if (// (!snapshot.exists()) &&
+            (password.length >= 6) &&
+            (rePassword === password) &&
+            (firstName.length > 4 || firstName.length < 32) &&
+            (lastName.length > 4 || lastName.length < 32) &&
+            (email.includes('@')
+            // || !snapshot.exists()
+            )) {
+            status.setLoginState(true);
+            user.setUsername(username);
+            user.setPassword(password);
+            user.setFirstName(firstName);
+            user.setLastName(lastName);
+            user.setEmail(email);
+            console.log(user);
+            navigate('../profile');
+        }
+    };
 
     return (
         <Flex
@@ -51,33 +77,45 @@ const SignUp = () => {
                     <Text as='cite'>...where home becomes art...</Text>
                 </Box>
                 <HStack h='350px' p={10}>
-                    <FormControl isRequired='true' pr={4}>
-                        <FormLabel>Username</FormLabel>
-                        <Input type='text' value={username} onChange={handleUsernameChange} bg='brand.600' color='brand.500' />
-                        <FormLabel>Password</FormLabel>
-                        <Input type='text' value={password.split('').map(() => '•').join('')} onChange={handlePasswordChange} bg='brand.600' color='brand.500' />
-                    </FormControl>
-                    <Divider orientation='vertical' />
-                    <FormControl isRequired='true' pl={4}>
-                        <FormLabel>First Name</FormLabel>
-                        <Input type='text' value={firstName} onChange={handleFirstNameChange} bg='brand.600' color='brand.500' />
-                        <FormLabel>Last Name</FormLabel>
-                        <Input type='text' value={lastName} onChange={handleLastNameChange} bg='brand.600' color='brand.500' />
-                        <FormControl isInvalid={isError} isRequired='true' >
-                            <FormLabel>Email</FormLabel>
-                            <Input type='email' value={email} onChange={handleEmailChange} bg='brand.600' color='brand.500'/>
-                            {isError && (
-                                <FormErrorMessage>Email is required.</FormErrorMessage>
-                            )}
+                    <VStack>
+                        <FormControl isInvalid={usernameError} isRequired='true' pr={4}>
+                            <FormLabel>Username</FormLabel>
+                            <Input type='text' placeholder='johndoe123' onChange={(e) => setUsername(e.target.value)} bg='brand.600' color='brand.500' />
+                            <FormErrorMessage>Username is taken.</FormErrorMessage>
                         </FormControl>
-                    </FormControl>
+                        <FormControl isInvalid={passwordError} isRequired='true' pr={4}>
+                            <FormLabel>Password</FormLabel>
+                            <Input type='text' onChange={(e) => setPassword(e.target.value)} bg='brand.600' color='brand.500' />
+                            <FormErrorMessage>Password should be more than 6 characters.</FormErrorMessage>
+                        </FormControl>
+                        <FormControl isInvalid={rePasswordError} isRequired='true' pr={4}>
+                            <FormLabel>Confirm</FormLabel>
+                            <Input type='text' onChange={(e) => setRePassword(e.target.value)} bg='brand.600' color='brand.500' />
+                            <FormErrorMessage>Password confirmation did not match.</FormErrorMessage>
+                        </FormControl>
+                    </VStack>
+                    <Divider orientation='vertical' />
+                    <VStack pl={4}>
+                        <FormControl isInvalid={firstNameError} isRequired='true'>
+                            <FormLabel>First Name</FormLabel>
+                            <Input type='text' placeholder='John' onChange={(e) => setFirstName(e.target.value)} bg='brand.600' color='brand.500' />
+                            <FormErrorMessage>First name should be between 4 and 32 characters.</FormErrorMessage>
+                        </FormControl>
+                        <FormControl isInvalid={lastNameError} isRequired='true'>
+                            <FormLabel>Last Name</FormLabel>
+                            <Input type='text' placeholder='Doe' onChange={(e) => setLastName(e.target.value)} bg='brand.600' color='brand.500' />
+                            <FormErrorMessage>Last name should be between 4 and 32 characters.</FormErrorMessage>
+                        </FormControl>
+                        <FormControl isInvalid={emailError} isRequired='true' >
+                            <FormLabel>Email</FormLabel>
+                            <Input type='email' placeholder='johndoe@interiorum.bg' onChange={(e) => setEmail(e.target.value)} bg='brand.600' color='brand.500'/>
+                            <FormErrorMessage>Email should be valid and unique.</FormErrorMessage>
+                        </FormControl>
+                    </VStack>
                 </HStack>
                 <VStack mb={8}>
                     <HStack>
-                        <Button colorScheme='orange' onClick={() => {
-                            user.setLoginState(true);
-                            navigate(from, { replace: true });
-                        }}>Sign Up</Button>
+                        <Button colorScheme='orange' onClick={handleSignUp}>Sign Up</Button>
                         <Button colorScheme='whiteAlpha' onClick={() => navigate('/')}>Cancel</Button>
                     </HStack>
                     <Text fontSize='sm'>Already have an account?
