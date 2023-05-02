da0c7ea8f62787a52c0c33bd40248641588018c1
Simona Ilcheva <sdmileva@gmail.com>
2023-05-02 18:03:44 +0300

    fix CategoriesList to style active category

--- interiorum-directory/interiorum/src/components/Views/Forum/CategoriesList.jsx
+++ interiorum-directory/interiorum/src/components/Views/Forum/CategoriesList.jsx
@@ -1,13 +1,14 @@
-import { Box, Heading, VStack, Text } from '@chakra-ui/react';
+import { Box, Heading, VStack, Link } from '@chakra-ui/react';
 import { categories } from '../../../../data';
 import { NavLink } from 'react-router-dom';
+import { BRAND_COLOR_3 } from '../../../common/constants';
 
 const CategoriesList = () => {
     return (
         <Box bg={'white'} borderRadius={'10px'} p={'20px'} m={'20px'} boxShadow={'md'}>
             <Heading as='h5' size='sm'>Categoreis List</Heading>
             <VStack ml={4} align='left' py={2}>
-                {categories.map(c => <Text key={c} fontSize='0.9em'><NavLink to={c}>{c}</NavLink></Text>)}
+                {categories.map(category => <Link key={category} as={NavLink} to={category} _activeLink={ { color: BRAND_COLOR_3 } }>{category}</Link>)}
             </VStack>
         </Box>
     );
