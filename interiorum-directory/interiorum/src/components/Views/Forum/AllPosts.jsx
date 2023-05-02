168c92110e2ed76bb170179352aa9b32ca6c793b
Simona Ilcheva <sdmileva@gmail.com>
2023-05-02 11:35:31 +0300

    add search functionality in Forum

--- interiorum-directory/interiorum/src/components/Views/Forum/AllPosts.jsx
+++ interiorum-directory/interiorum/src/components/Views/Forum/AllPosts.jsx
@@ -1,17 +1,38 @@
-import { Heading, Box } from '@chakra-ui/react';
+import { Heading, Box, HStack, Input, Button, Text } from '@chakra-ui/react';
 
 import { posts } from '../../../../data.js';
 import ForumTabs from './ForumTabs.jsx';
+import { useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 
 const AllPosts = () => {
 
     // fetch all posts
 
+    const [searchTerm, setSerchTerm] = useState('');
+    const [searchParams, setSearchParams] = useSearchParams();
+
+
     return (
         <Box bg={'white'} borderRadius={'10px'} p={'20px'} m={'20px'} boxShadow={'md'}>
             <Heading size='xl' mb='15px'>All posts</Heading>
-            <ForumTabs posts={posts} />
+
+            <HStack>
+                <Input
+                    onChange={(e) => setSerchTerm(e.target.value)}
+                    value={searchTerm}
+                    m='0px 0px 10px'
+                    bg='white'
+                    focusBorderColor='brand.400'
+                    placeholder='Search all posts' />
+                <Button onClick={() => {
+                    setSearchParams({ search: searchTerm.toLocaleLowerCase() });
+                    setSerchTerm('');
+                }}>Search</Button>
+            </HStack>
+            <>{searchParams.get('search') ? (<Text p='15px' fontStyle='italic'>Search results for "{searchParams.get('search')}"</Text>) : null}</>
+            <ForumTabs posts={searchParams.get('search') ? posts.filter(post => post.title.toLocaleLowerCase().includes(searchParams.get('search'))) : posts} />
         </Box>
     );
 };
--- interiorum-directory/interiorum/src/components/Views/Forum/CategoryPosts.jsx
+++ interiorum-directory/interiorum/src/components/Views/Forum/CategoryPosts.jsx
@@ -1,14 +1,18 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useSearchParams } from 'react-router-dom';
 
-import { Heading, Text, Box } from '@chakra-ui/react';
+import { Heading, Text, Box, HStack, Input, Button } from '@chakra-ui/react';
 
 import { posts } from '../../../../data';
 import ForumTabs from './ForumTabs';
+import { useState } from 'react';
 
 
 const CategoryPosts = () => {
 
     const { category } = useParams();
+
+    const [searchTerm, setSerchTerm] = useState('');
+    const [searchParams, setSearchParams] = useSearchParams();
 
     const categoryPosts = posts.filter(post => post.category === category); // fetch posts by category
 
@@ -16,7 +20,23 @@ const CategoryPosts = () => {
         <Box bg={'white'} borderRadius={'10px'} p={'20px'} m={'20px'} boxShadow={'md'}>
             <Text fontStyle='italic'>posts from category</Text>
             <Heading size='xl' mb='15px'>{category}</Heading>
-            <ForumTabs posts={categoryPosts} />
+
+            <HStack>
+                <Input
+                    onChange={(e) => setSerchTerm(e.target.value)}
+                    value={searchTerm}
+                    m='0px 0px 10px'
+                    bg='white'
+                    focusBorderColor='brand.400'
+                    placeholder={`Search posts in category ${category}`} />
+                <Button onClick={() => {
+                    setSearchParams({ search: searchTerm.toLocaleLowerCase() });
+                    setSerchTerm('');
+                }}>Search</Button>
+            </HStack>
+            <>{searchParams.get('search') ? (<Text p='15px' fontStyle='italic'>Search results for "{searchParams.get('search')}"</Text>) : null}</>
+
+            <ForumTabs posts={searchParams.get('search') ? categoryPosts.filter(post => post.title.toLocaleLowerCase().includes(searchParams.get('search'))) : categoryPosts} />
         </Box>
     );
 
