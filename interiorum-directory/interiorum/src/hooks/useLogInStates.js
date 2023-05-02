aa112253709322c53276899bb13ac50008093d0a
samu.sar <samuil.sarandev@gmail.com>
2023-05-02 13:55:48 +0300

    Create custom hook for initializing login states

---
+++ interiorum-directory/interiorum/src/hooks/useLogInStates.js
@@ -0,0 +1,38 @@
+import { useContext, useState } from 'react';
+import { AuthContext } from '../context/AuthContext/AuthContext';
+import { UserContext } from '../context/UserContext/UserContext';
+import { useNavigate } from 'react-router-dom';
+import { useToast } from '@chakra-ui/react';
+
+const useLogInStates = () => {
+    const status = useContext(AuthContext);
+    const user = useContext(UserContext);
+
+    const [username, setUsername] = useState('');
+    const [usernameError, setUsernameError] = useState(false);
+    const [password, setPassword] = useState('');
+    const [passwordError, setPasswordError] = useState(false);
+
+    const navigate = useNavigate();
+    const toast = useToast();
+    const [show, setShow] = useState(false);
+
+    return {
+        status,
+        user,
+        username,
+        setUsername,
+        usernameError,
+        setUsernameError,
+        password,
+        setPassword,
+        passwordError,
+        setPasswordError,
+        navigate,
+        toast,
+        show,
+        setShow,
+    };
+};
+
+export default useLogInStates;
