81e41ad81b8e66af956171436425aeb5263caa43
samu.sar <samuil.sarandev@gmail.com>
2023-05-05 21:33:53 +0300

    Add safeguard for Forum rendering

--- interiorum-directory/interiorum/src/components/Views/Forum/Forum.jsx
+++ interiorum-directory/interiorum/src/components/Views/Forum/Forum.jsx
@@ -4,24 +4,30 @@ import CreatePostHeader from './CreatePostHeader';
 import CategoriesList from './CategoriesList';
 import TopCommentersList from './TopCommentersList';
 import { Outlet } from 'react-router-dom';
+import { useContext } from 'react';
+import { AppContext } from '../../../context/AppContext/AppContext';
 
 const Forum = () => {
-    return (
-        <Box className="forum-view">
-            <CreatePostHeader />
-            <Grid className='forum-content' templateColumns='1fr 4fr'>
-                <GridItem>
-                    <CategoriesList />
-                    <TopCommentersList />
-                </GridItem>
-                <GridItem>
-                    <Outlet />
-                </GridItem>
+    const { userData } = useContext(AppContext);
 
-            </Grid>
+    if (userData) {
+        return (
+            <Box className="forum-view">
+                <CreatePostHeader />
+                <Grid className='forum-content' templateColumns='1fr 4fr'>
+                    <GridItem>
+                        <CategoriesList />
+                        <TopCommentersList />
+                    </GridItem>
+                    <GridItem>
+                        <Outlet />
+                    </GridItem>
 
-        </Box>
-    );
+                </Grid>
+
+            </Box>
+        );
+    }
 };
 
 export default Forum;
