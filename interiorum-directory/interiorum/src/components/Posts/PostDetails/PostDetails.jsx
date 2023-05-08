45d38c2394810a35d7edb71636d52e2bb7ca3837
samu.sar <samuil.sarandev@gmail.com>
2023-05-08 17:04:41 +0300

    Add PostDetails author fetching and category/tags

--- interiorum-directory/interiorum/src/components/Posts/PostDetails/PostDetails.jsx
+++ interiorum-directory/interiorum/src/components/Posts/PostDetails/PostDetails.jsx
@@ -3,26 +3,37 @@ import { Link, useParams } from 'react-router-dom';
 import { AiOutlineLike, AiFillLike } from 'react-icons/ai';
 import { FaRegComment } from 'react-icons/fa';
 import { FiShare } from 'react-icons/fi';
+import { useEffect, useState } from 'react';
+import { getUserByHandle } from '../../../services/users.service';
 
 const PostDetails = ({ post }) => {
+    const [author, setAuthor] = useState(null);
+
+    useEffect(() => {
+        getUserByHandle(post.author)
+            .then(snapshot => snapshot.val())
+            .then(data => setAuthor(data))
+            .catch(error => console.log('Fetching author data was unsuccessful: ' + error.message));
+    }, []);
 
     return (
         <>
             <VStack p={8} bg='brand.100' rounded='md' w={{ sm: '100%', md: '80%' }} boxShadow='lg'>
-                <HStack w='80%' justify='start' py={3} fontWeight='500' color='brand.300'>
-                    <Link to='../forum'>Forum</Link>
-                    <Text> / </Text>
-                    <Link to={`../forum/${post.category}`}>{post.category}</Link>
-                </HStack>
                 <HStack w='100%' align='start' justify='left'>
-                    <Image src='https://bit.ly/dan-abramov' w='70px' rounded='full'mx={2}/>
+                    <Image
+                        src={author && author.avatarURL}
+                        fallbackSrc='https://firebasestorage.googleapis.com/v0/b/interiorum-6c515.appspot.com/o/assets%2Fanon-user.jpg?alt=media&token=0007d79f-52fb-4866-9747-326d52395bd9'
+                        w='70px'
+                        rounded='full'
+                        mx={2}/>
                     <VStack align='start' w='80%'>
                         <VStack align='start' w='100%'>
-                            <Link to={`/forum/${post.category}`}><Badge>{post.category}</Badge></Link>
-
+                            <HStack>
+                                {post.categories && post.categories.map(category => <Link key={category} to={`/forum/${category}`}><Badge>{category}</Badge></Link>)}
+                            </HStack>
                             <Heading as='h1' size='lg' fontWeight='500' w='100%'>{post.title}</Heading>
                             <HStack>
-                                <Link>{post.author}</Link>
+                                <Link to={`../../profile/${post.author}`}>{post.author}</Link>
                                 <Text>| {post.createdOn}</Text>
                             </HStack>
                             <Divider w='100%' />
