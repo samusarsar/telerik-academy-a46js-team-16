f7ce5f8446b28aeef1397492de64d2163b835f2f
samu.sar <samuil.sarandev@gmail.com>
2023-05-08 17:03:06 +0300

    Add condition for IndividualPost rendering

--- interiorum-directory/interiorum/src/components/Views/IndividualPost/IndividualPost.jsx
+++ interiorum-directory/interiorum/src/components/Views/IndividualPost/IndividualPost.jsx
@@ -4,27 +4,34 @@ import { useParams } from 'react-router-dom';
 import { Spacer, VStack } from '@chakra-ui/react';
 import PostCommentsBox from '../../Posts/PostCommentsBox/PostCommentsBox';
 import { getPostById } from '../../../services/post.service';
+import { getCommentsByPost } from '../../../services/comment.services';
 
 const IndividualPost = () => {
 
     // const [loading, setLoading] = useState(false);
-    const [post, setPost] = useState([]);
+    const [post, setPost] = useState(null);
+    const [comments, setComments] = useState([]);
 
     const { postId } = useParams();
 
     useEffect(() => {
         getPostById(postId)
             .then(result => setPost(result));
+
+        // getCommentsByPost(postId)
+        //     .then(snapshot => snapshot.val())
+        //     .then(data => setComments(data ? Object.values(data) : []));
     }, []);
 
-
-    return (
-        <VStack align='center' bg='brand.600' py={3}>
-            <PostDetails post={post} />
-            <Spacer />
-            <PostCommentsBox comments={post.comments || []} />
-        </VStack>
-    );
+    if (post && comments) {
+        return (
+            <VStack align='center' bg='brand.600' py={3}>
+                <PostDetails post={post} />
+                <Spacer />
+                <PostCommentsBox comments={post.comments || []} />
+            </VStack>
+        );
+    }
 };
 
 export default IndividualPost;
