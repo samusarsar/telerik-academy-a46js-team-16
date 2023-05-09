ead0a56d4ec819ca50ec2acac8a98970ad5931f5
samu.sar <samuil.sarandev@gmail.com>
2023-05-09 15:12:46 +0300

    Fix inProfile state setting logic

--- interiorum-directory/interiorum/src/components/Base/NavBar/NavBar.jsx
+++ interiorum-directory/interiorum/src/components/Base/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import { NavLink, useNavigate } from 'react-router-dom';
+import { NavLink, useNavigate, useParams } from 'react-router-dom';
 
 import { Heading, Spacer, HStack, Button, Image, Menu, MenuButton, MenuList, MenuItem, MenuDivider, useDisclosure, Box, useToast, Link, Text } from '@chakra-ui/react';
 
@@ -15,27 +15,30 @@ const NavBar = () => {
     const [inProfile, setInProfile] = useState(false);
 
     const navigate = useNavigate();
-
+    const { handle } = useParams();
     const toast = useToast();
 
     const { isOpen, onOpen, onClose } = useDisclosure();
 
     useEffect(() => {
         if (userData) {
-            onValue(ref(db, `users/${userData.handle}/avatarURL`), (snapshot) => {
+            return onValue(ref(db, `users/${userData.handle}/avatarURL`), (snapshot) => {
                 const data = snapshot.val();
                 setMenuAvatar(data);
             });
         }
     });
 
+    useEffect(() => {
+        if (userData) {
+            setInProfile(userData.handle === handle);
+        }
+    }, [handle]);
+
     return (
         <>
             <HStack as='nav' className="navbar" bg='white' wrap='wrap' spacing={30} p={4}>
-                <HStack _hover={{ cursor: 'pointer' }} onClick={() => {
-                    navigate('home');
-                    inProfile && setInProfile(false);
-                }}>
+                <HStack _hover={{ cursor: 'pointer' }} onClick={() => navigate('home')} >
                     <Image
                         src='https://firebasestorage.googleapis.com/v0/b/interiorum-6c515.appspot.com/o/assets%2Flogo.png?alt=media&token=dfd1dd62-93fc-4de0-a77f-4e941c4e09a5'
                         boxSize='80px'/>
@@ -43,14 +46,11 @@ const NavBar = () => {
                 </HStack>
                 <Spacer />
                 <Link as={NavLink} rounded='lg' px={2} py={2} to='home' _activeLink={{ fontWeight: 'bold', color: 'brand.300' }}
-                    _hover={{ textDecoration: 'none', bg: 'blackAlpha.200' }}
-                    onClick={() => inProfile && setInProfile(false)}>Home</Link>
+                    _hover={{ textDecoration: 'none', bg: 'blackAlpha.200' }}>Home</Link>
                 <Link as={NavLink} rounded='lg' px={2} py={2} to='forum' _activeLink={{ fontWeight: 'bold', color: 'brand.300' }}
-                    _hover={{ textDecoration: 'none', bg: 'blackAlpha.200' }}
-                    onClick={() => inProfile && setInProfile(false)}>Forum</Link>
+                    _hover={{ textDecoration: 'none', bg: 'blackAlpha.200' }}>Forum</Link>
                 <Link as={NavLink} rounded='lg' px={2} py={2} to='about' _activeLink={{ fontWeight: 'bold', color: 'brand.300' }}
-                    _hover={{ textDecoration: 'none', bg: 'blackAlpha.200' }}
-                    onClick={() => inProfile && setInProfile(false)}>About</Link>
+                    _hover={{ textDecoration: 'none', bg: 'blackAlpha.200' }}>About</Link>
                 {(user && userData) ? (
                     <Box
                         borderRadius='xl'
@@ -62,10 +62,7 @@ const NavBar = () => {
                         transition='.1s ease-in-out'
                         _hover={{ textDecoration: 'none', bg: `${inProfile ? 'brand.400' : 'brand.100'}`, color: `${inProfile ? 'brand.600' : 'brand.500'}` }}>
                         <Menu autoSelect={false} isLazy={true} isOpen={isOpen} unmount>
-                            <MenuButton minW='fit-content' px={3} onMouseEnter={onOpen} onClick={() => {
-                                navigate('my-profile');
-                                setInProfile(true);
-                            }}>
+                            <MenuButton minW='fit-content' px={3} onMouseEnter={onOpen} onClick={() => navigate('my-profile')}>
                                 <HStack gap={2}>
                                     <Image src={menuAvatar}
                                         fallbackSrc='https://firebasestorage.googleapis.com/v0/b/interiorum-6c515.appspot.com/o/assets%2Fanon-user.jpg?alt=media&token=0007d79f-52fb-4866-9747-326d52395bd9'
@@ -77,10 +74,7 @@ const NavBar = () => {
                             <MenuList>
                                 <MenuItem color='gray.700' transition='.1s ease-in-out'
                                     _hover={{ bg: 'blackAlpha.100' }}
-                                    onClick={() => {
-                                        navigate('my-profile');
-                                        !inProfile && setInProfile(true);
-                                    }}>My Profile</MenuItem>
+                                    onClick={() => navigate('my-profile')}>My Profile</MenuItem>
                                 <MenuDivider />
                                 <MenuItem color='brand.300' transition='.1s ease-in-out'
                                     _hover={{ bg: 'brand.300', color: 'brand.600' }} onClick={() => handleLogOut({ setContext, onClose, navigate, toast })}>
@@ -90,7 +84,6 @@ const NavBar = () => {
                         </Menu>
                     </Box>) : (
                     <>
-                        {inProfile && setInProfile(false)}
                         <LogInModal />
                         <Button colorScheme='orange' onClick={() => navigate('sign-up')}>Sign up</ Button>
                     </>
