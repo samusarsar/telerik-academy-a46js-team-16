9db436529733b1697856045a625bfd25bb05dd6e
samu.sar <samuil.sarandev@gmail.com>
2023-05-09 15:11:06 +0300

    Add like/unlike to SinglePost component

--- interiorum-directory/interiorum/src/components/Posts/SinglePost/SinglePost.jsx
+++ interiorum-directory/interiorum/src/components/Posts/SinglePost/SinglePost.jsx
@@ -34,24 +34,26 @@ const SinglePost = ({ post, large=false }) => {
                 <Heading as='h5' size='sm'><Link to={`../../post/${post.postId}`}>{post.title}</Link></Heading>
 
                 {large && <Text fontSize='sm' my={3}>{body}</Text>}
-                <HStack justify='left' py={2}>
+                <HStack justify='right' py={2} flexWrap='wrap'>
                     <Text fontSize='0.8em'>Posted by <Link to={`../../profile/${post.author}`}><b>{post.author}</b></Link></Text>
                     <Text fontSize='0.8em' color='gray.500'>On {post.createdOn}</Text>
                     <Spacer/>
-                    {(postLikes) &&
-                            <Button h='25px' p={1} fontSize='0.8em' colorScheme={!isLiked ? 'blackAlpha' : 'telegram'} onClick={() => {
-                                !isLiked ?
-                                    handleLikePost({ postId: post.postId, handle: userData.handle }) :
-                                    handleUnlikePost({ postId: post.postId, handle: userData.handle });
-                            }}><Icon as={isLiked ? AiFillLike : AiOutlineLike} mr={1} />Like{postLikes.length ? ` | ${postLikes.length}` : ''}
-                            </Button>}
-                    <AvatarGroup size='sm' max={3} fontSize='0.8em' spacing='-0.5rem' >
-                        <Avatar name='Ryan Florence' src='https://bit.ly/ryan-florence' />
-                        <Avatar name='Segun Adebayo' src='https://bit.ly/code-beast' />
-                        <Avatar name='Kent Dodds' src='https://bit.ly/kent-c-dodds' />
-                        <Avatar name='Prosper Otemuyiwa' src='https://bit.ly/prosper-baba' />
-                        <Avatar name='Christian Nwamba' src='https://bit.ly/code-beast' />
-                    </AvatarGroup>
+                    <HStack>
+                        {(postLikes) &&
+                                <Button h='25px' p={1} fontSize='0.8em' colorScheme={!isLiked ? 'blackAlpha' : 'telegram'} onClick={() => {
+                                    !isLiked ?
+                                        handleLikePost({ postId: post.postId, handle: userData.handle }) :
+                                        handleUnlikePost({ postId: post.postId, handle: userData.handle });
+                                }}><Icon as={isLiked ? AiFillLike : AiOutlineLike} mr={1} />Like{postLikes.length ? ` | ${postLikes.length}` : ''}
+                                </Button>}
+                        <AvatarGroup size='sm' max={3} fontSize='0.8em' spacing='-0.5rem' >
+                            <Avatar name='Ryan Florence' src='https://bit.ly/ryan-florence' />
+                            <Avatar name='Segun Adebayo' src='https://bit.ly/code-beast' />
+                            <Avatar name='Kent Dodds' src='https://bit.ly/kent-c-dodds' />
+                            <Avatar name='Prosper Otemuyiwa' src='https://bit.ly/prosper-baba' />
+                            <Avatar name='Christian Nwamba' src='https://bit.ly/code-beast' />
+                        </AvatarGroup>
+                    </HStack>
                 </HStack>
             </Box>
         );
