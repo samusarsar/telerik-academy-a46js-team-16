5f505db231c74884e614259392ad72965291612f
samu.sar <samuil.sarandev@gmail.com>
2023-05-10 11:52:18 +0300

    Add link to forum from MostRecent/Commented

--- interiorum-directory/interiorum/src/components/Posts/PostsBox/PostsBox.jsx
+++ interiorum-directory/interiorum/src/components/Posts/PostsBox/PostsBox.jsx
@@ -1,14 +1,17 @@
 import { Container, Heading, Button, Icon } from '@chakra-ui/react';
 import { AiOutlineArrowRight } from 'react-icons/ai';
-
 import PostFeed from '../../Posts/PostFeed/PostFeed.jsx';
 
+import { useNavigate } from 'react-router-dom';
+
 const PostsBox = ({ heading, posts }) => {
+    const navigate = useNavigate();
+
     return (
         <Container display={'flex'} flexDirection={'column'} align={'center'} bg={'white'} borderRadius={'10px'} p={'20px'} m={'20px'} boxShadow={'2xl'}>
             <Heading size='md'>{heading}</Heading>
             <PostFeed posts={posts}></PostFeed>
-            <Button rightIcon={<Icon as={AiOutlineArrowRight} />} colorScheme='telegram' variant='outline'>
+            <Button rightIcon={<Icon as={AiOutlineArrowRight} />} colorScheme='telegram' variant='outline' onClick={() => navigate('../forum')}>
                     View all
             </Button>
         </Container>
